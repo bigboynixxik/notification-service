0907847131619b766f1cd7ba67acc197c80c935d
Nixxik <v.gorbachev05@yandex.ru>
2026-04-21 18:51:21 +0700

    feat add main.go

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"context"
+	"log"
+	"notification-service/internal/app"
+)
+
+func main() {
+	ctx := context.Background()
+	app, err := app.NewApp(ctx)
+	if err != nil {
+		panic(err)
+	}
+	if err := app.Run(ctx); err != nil {
+		log.Fatal(err)
+	}
+}
